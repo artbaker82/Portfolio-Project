00243de680b12f4e9a5cf8413b7900860e063b54
Arthur Baker <artbaker82@gmail.com>
2021-06-17 19:39:46 -0700

    Responsive Nav and feature content cards

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -5,6 +5,8 @@ window.onscroll = function () {
 function scrollfunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     document.getElementById("navbar").classList.add("bg-dark");
+    document.getElementById("navbar").classList.add("navbar-dark");
+    document.getElementById("navbar").classList.remove("navbar-light");
   } else {
     document.getElementById("navbar").classList.remove("bg-dark");
   }
